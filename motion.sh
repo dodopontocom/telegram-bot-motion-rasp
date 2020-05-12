037b2c8b4fac107246f7d795d4ae9601d97c90ea
rodolfotiago <rodolfotiago@gmail.com>
2020-05-12 15:24:37 -0300

    debug

--- motion.sh
+++ motion.sh
@@ -20,16 +20,12 @@ curl_cmd() {
                  ${api_url}/bot${TELEGRAM_TOKEN}/sendMessage &> /dev/null
 
 		curl --silent -F document=@"${file}" ${api_url}/bot${TELEGRAM_TOKEN}/sendDocument?chat_id=${NOTIFICATION_ID} &> /dev/null
-		
-		message="Arquivo enviado com sucesso! ${file}"
-		echo "${message}"
 	else
 		message="Nenhum arquivo encontrado no momento"
 		# curl --silent -X POST \
         #         -d chat_id=${NOTIFICATION_ID} \
         #         -d text="${message}" \
         #         ${api_url}/bot${TELEGRAM_TOKEN}/sendMessage &> /dev/null
-		echo "${message}"
 	fi
 }
 
@@ -41,24 +37,31 @@ which motion || echo -e "==============\nFirst run:\nsudo apt-get install -y mot
 # Adding the custom config file
 if [[ ! -f ${send_file_path} ]]; then
 	mkdir -p ${send_file_path}
+	echo "==============\nDiretório do motion criado!!!\n=============="
 	cp ${config_file} ${send_file_path}
+	echo "==============\nArquivo de configuração copiado!\n=============="
 else
 	cp ${config_file} ${send_file_path}
+	echo "==============\nArquivo de configuração copiado!\n=============="
 fi
 
 # Run Motion in daemon mode with custom config file
 if [[ ! $(ps -ef | grep "motion" | grep -v grep) ]]; then
 	/usr/bin/motion -b -c ${send_file_path}/motion.conf
+	echo "==============\nMotion inicializado!\n=============="
 fi
 
 file_extension="mkv"
 
 has_file=($(find ${send_file_path} -name "*.${file_extension}"))
 if [[ -n ${has_file} ]]; then
+	echo "==============\nArquivos encontrados!\n=============="
 	for f in ${has_file[@]}; do
 		curl_cmd ${f}
+		echo "==============\nNotificação enviada para o usuário!\n=============="
 		mv ${f} ${f}.disabled
 	done
 else
+	echo "==============\nNenhum arquivo encontrado no momento\n=============="
 	curl_cmd
 fi
