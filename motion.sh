b92136b707138f3fb50e60a6cd1647daa658bece
dodopontocom <rodolfotiago@gmail.com>
2022-04-05 14:15:29 -0300

    a few adjusts

--- motion.sh
+++ motion.sh
@@ -8,6 +8,18 @@ config_file="${BASEDIR}/config/motion.conf"
 send_file_path="/home/pi/.motion"
 api_url="https://api.telegram.org"
 
+file_extension="mkv"
+
+exitOnError() {
+  # usage: exitOnError <output_message> [optional: code (defaul:exit code)]
+  code=${2:-$?}
+  if [[ $code -ne 0 ]]; then
+      if [ ! -z "$1" ]; then echo -e "ERROR: $1" >&2 ; fi
+      echo "Exiting..." >&2
+      exit $code
+  fi
+}
+
 # Send the file command
 curl_cmd() {
 	local file message
@@ -30,9 +42,9 @@ curl_cmd() {
 }
 
 # Looking for dependencies
-lsusb | grep -i webcam || echo "==============\nVerifique se a webcam está bem conectada!!!\n=============="
-which curl || echo -e "==============\nFirst run:\nsudo apt-get install -y curl\n=============="
-which motion || echo -e "==============\nFirst run:\nsudo apt-get install -y motion\n=============="
+lsusb | grep -i webcam || exitOnError "==============\nVerifique se a webcam está bem conectada!!!\n=============="
+which curl || exitOnError "==============\nFirst run:\nsudo apt-get install -y curl\n=============="
+which motion || exitOnError "==============\nFirst run:\nsudo apt-get install -y motion\n=============="
 
 # Adding the custom config file
 if [[ ! -f ${send_file_path} ]]; then
@@ -50,8 +62,6 @@ if [[ ! -f ${send_file_path}/motion.pid ]]; then
 	/usr/bin/motion -b -c ${send_file_path}/motion.conf
 	echo "==============\nMotion inicializado!\n=============="
 fi
-
-file_extension="mkv"
 
 has_file=($(find ${send_file_path} -name "*.${file_extension}"))
 if [[ -n ${has_file} ]]; then
